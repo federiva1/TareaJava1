bebb8acafb014a6f48bdb9f65ef2afb45873d3a8
federiva1 <federico.rivadeneira92@gmail.com>
2023-03-02 21:55:23 -0300

    tercera_entrega

---
+++ principal.js
@@ -0,0 +1,14 @@
+let Seccion = [];
+
+const btnAuto = document.getElementById("BotonAuto");
+const btnMoto = document.getElementById("BotonMoto");
+
+function agregarUsuario(evento) {
+  const ramo = evento.target.id === "BotonAuto" ? "Auto" : "Moto";
+  const usuario = { ramo };
+  Seccion.push(usuario);
+  localStorage.setItem("Seccion", JSON.stringify(Seccion));
+}
+
+btnAuto.addEventListener("click", agregarUsuario);
+btnMoto.addEventListener("click", agregarUsuario);
